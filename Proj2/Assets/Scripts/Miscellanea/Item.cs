0a996fb90e2944867b8f7a56bdced7bbde122b46
guplem <guplem@gmail.com>
2019-04-07 21:41:12 +0200

    Folder's reorganization

--- Proj2/Assets/Scripts/Miscellanea/Item.cs
+++ Proj2/Assets/Scripts/Miscellanea/Item.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Item : MonoBehaviour
 {
 
-    public Sprite sprite;
-    public PhysicMaterial physicMaterial;
+    //public Sprite sprite;
+    //public PhysicMaterial physicMaterial;
     public Collider2D pickUpCollider;
 
 }
