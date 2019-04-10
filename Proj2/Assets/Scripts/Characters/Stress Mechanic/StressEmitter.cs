9dd46761b155384411fac0eb44d5a5332dfd1a80
Crynano <pa.crysis@gmail.com>
2019-04-10 18:07:26 +0200

    Added Stress Mechanic folders and Emmiter / Manager

---
+++ Proj2/Assets/Scripts/Characters/Stress Mechanic/StressEmitter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressEmitter
+{
+    private Vector3 emittingPoint;
+    private float radius;
+
+    public StressEmitter(Vector3 emittingPoint)
+    {
+        this.emittingPoint = emittingPoint;
+    }
+
+    public void EmitStressBurst(int amount)
+    {
+        StressManager[] temp = SearchForReceivers();
+        if (temp.Length < 1)
+            return;
+        foreach (StressManager stressManager in temp)
+        {
+            stressManager.AddStress(amount);
+        }
+
+    }
+
+    public void AddStressOverTime(int amount, float cooldown)
+    {
+        StressManager[] temp = SearchForReceivers();
+        if (temp.Length < 1)
+            return;
+        StartCoroutine(AddStressToTargetsOverTime(temp, amount, cooldown));
+
+    }
+
+    public StressManager[] SearchForReceivers()
+    {
+        StressManager[] stressManagerArray;
+
+        Collider2D[] colArray = Physics2D.OverlapCircle(emittingPoint, radius, GameManager.Instance.playerLayer);
+
+        if (colArray.Length < 1)
+            return null;
+
+        stressManagerArray = new StressManager[colArray.Length];
+
+        for (int i = 0; i < colArray.Length - 1; i++)
+        {
+            try
+            {
+                stressManagerArray[i] = colArray[i].GetComponent<PlayerManager>().stressManager;
+            }
+            catch
+            {
+                Debug.Log("Doesn't have StressManager");
+            }
+        }
+        return stressManagerArray;
+    }
+
+    IEnumerator AddStressToTargetsOverTime(StressManager[] targets, int amount, float cooldown)
+    {
+        foreach (StressManager target in targets)
+        {
+            target.AddStress();
+        }
+        
+    }
+}
---
+++ Proj2/Assets/Scripts/Characters/Stress Mechanic/StressManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressManager
+{
+    private int currentStress;
+    private int stressThreshold;
+
+    private PlayerManager playerManager;
+
+    public StressManager(PlayerManager playerManager)
+    {
+        this.playerManager = playerManager;
+    }
+
+    public bool AddStress(int amount)
+    {
+        currentStress += amount;
+        if (currentStress >= stressThreshold)
+            SomethingHappens();
+        return true;
+    }
+
+    public void SomethingHappens()
+    {
+        //TODO idea is to make the player random movements, thingys or fuck-ups.
+
+    }
+}
