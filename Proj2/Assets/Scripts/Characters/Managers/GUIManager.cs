c0ece034515c7913eea6d7d7881f8f9521e695f2
gillemp <guplem@gmail.com>
2019-04-24 19:02:18 +0200

    GUI Basics

---
+++ Proj2/Assets/Scripts/Characters/Managers/GUIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject backgroundVignette;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject controlsPanel;
+
+    [HideInInspector] private Image backgroundVignetteImg;
+
+    [HideInInspector] private static GUIManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Debug.LogError("There are multiple instances of GUIManager.", gameObject);
+        else
+            Instance = this;
+    }
+
+    private void Start()
+    {
+        backgroundVignetteImg = backgroundVignette.GetComponent<Image>();
+    }
+
+    public void QuitPauseMenu()
+    {
+        //TODO
+        Debug.Log("Clicked resume game button");
+        pausePanel.SetActive(false);
+    }
+
+    public void PauseMenu(bool show)
+    {
+        pausePanel.SetActive(show);
+        Controls(show);
+    }
+
+    public void Controls(bool show)
+    {
+        pausePanel.SetActive(show);
+    }
+
+    public void ExitGame()
+    {
+        //TODO
+        Debug.Log("Clicked exit game button");
+    }
+
+    public void SetVignetteOpacity(float opacityPercentage)
+    {
+        //TODO
+    }
+
+}
