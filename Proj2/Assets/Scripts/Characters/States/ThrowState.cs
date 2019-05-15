6fa08f56a031391a939c0060a46b620f98497985
Crynano <pa.crysis@gmail.com>
2019-05-15 17:08:51 +0200

    Throwing improved and limited.

--- Proj2/Assets/Scripts/Characters/States/ThrowState.cs
+++ Proj2/Assets/Scripts/Characters/States/ThrowState.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class ThrowState : State
 {
-
+    bool lookingRight;
     public ThrowState(PlayerManager characterManager)
     {
         this.character = characterManager;
         ((PlayerManager)character).rb2d.velocity = Vector3.zero;
+        lookingRight = character.transform.eulerAngles == new Vector3(0, 0, 0);
     }
 
     protected override IEnumerator StartState()
@@ -29,7 +30,12 @@ public class ThrowState : State
 
     private void CheckThrow()
     {
+        lookingRight = character.transform.eulerAngles == new Vector3(0, 0, 0);
+
+        Vector3 mousePosition = GameManager.Instance.cursor.GetCursorPositionOnWorld();
         Vector3 throwPosition = ((PlayerManager)character).getThrowPoint().position;
+
+        float mousePositionCorrected = mousePosition.x;
 
         if (character.brain.actionHold)
         {
@@ -40,37 +46,38 @@ public class ThrowState : State
                 return;
             }
 
-            Vector3 mousePosition = GameManager.Instance.cursor.GetCursorPositionOnWorld();
+            if (lookingRight)
+            {
+                if (mousePosition.x < throwPosition.x)
+                    mousePositionCorrected = throwPosition.x;
 
-            Vector3[] points = { throwPosition, mousePosition };
+                if (mousePosition.x < character.transform.position.x)
+                    Utils.SetObjectLookingDirection(0, character.gameObject);
+            }
+            else
+            {
+                if (mousePosition.x > throwPosition.x)
+                    mousePositionCorrected = throwPosition.x;
+
+                if (mousePosition.x > character.transform.position.x)
+                {
+                    Utils.SetObjectLookingDirection(1, character.gameObject);
+                }
+            }
+            Vector3[] points = { throwPosition, new Vector3(mousePositionCorrected, mousePosition.y, mousePosition.z) };
 
             GameManager.Instance.lineManager.SetupLinePoints(points);
             GameManager.Instance.lineManager.SetDrawing(true);
-
-            if (mousePosition.x < throwPosition.x)
-            {
-                Utils.SetObjectLookingDirection(0, character.gameObject);
-            }
-            else if (mousePosition.x > throwPosition.x)
-            {
-                Utils.SetObjectLookingDirection(1, character.gameObject);
-            }
-            Debug.Log("Aiming!");
         }
-        else // Nota: és necessari controla actionRelease? no serveix !brain.action?
+        else
         {
             // Play animation!
             //character.visualsAnimator.SetTrigger("Throw");
 
-            Vector3 direction;
-            Vector3 mousePosition = GameManager.Instance.cursor.GetCursorPositionOnWorld();
-
-            direction = mousePosition - throwPosition;
-
-            ((PlayerManager)character).inventory.ThrowStoredItem(new Vector2(((PlayerManager)character).throwingForce.x, ((PlayerManager)character).throwingForce.y) * direction, throwPosition);
+            Vector3 direction = (new Vector3(mousePositionCorrected, mousePosition.y, mousePosition.z) - throwPosition);
+            ((PlayerManager)character).inventory.ThrowStoredItem(((PlayerManager)character).throwingForce * direction, throwPosition);
 
             ((PlayerManager)character).behaviourTree.CalculateAndSetNextState(true);
-            Debug.Log("THROW!");
         }
     }
 
