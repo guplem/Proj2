b5e768f439475fb321d99c55f17206ca7b19d5e7
gillemp <guplem@gmail.com>
2019-05-21 17:58:34 +0200

    Elevator

--- Proj2/Assets/Scripts/InWorldElements/Activables/Activable.cs
+++ Proj2/Assets/Scripts/InWorldElements/Activables/Activable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public abstract class Activable : MonoBehaviour
 {
 
---
+++ Proj2/Assets/Scripts/InWorldElements/Activables/Elevator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : Activable
+{
+    [SerializeField] private float travelPointOn;
+    [SerializeField] private float travelPointOff;
+    private float targetPoint;
+    private float velocity = 0.02f;
+
+
+    public override ActivationType GetActivationType()
+    {
+        return ActivationType.Other;
+    }
+
+    protected override void SetState(bool state, CharacterManager characterActivating, bool alertAtActivate)
+    {
+        targetPoint = state ? travelPointOn : travelPointOff;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(new Vector3(transform.position.x, travelPointOn, 0f), 0.2f);
+        Gizmos.DrawSphere(new Vector3(transform.position.x, travelPointOff, 0f), 0.2f);
+    }
+
+    private void FixedUpdate()
+    { 
+
+        float distance = targetPoint - transform.position.y;
+
+        if (Mathf.Abs(distance) < velocity)
+            return;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + (velocity*Mathf.Sign(distance)), transform.position.z);
+
+    }
+}
