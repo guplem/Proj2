24f07418021bac4fb5496cfc7eabc3451f00d6f7
Crynano <pa.crysis@gmail.com>
2019-05-22 19:07:22 +0200

    Metal Detector! and some fixes...

---
+++ Proj2/Assets/Scripts/InWorldElements/Activables/ActivableMetalDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivableMetalDetector : Activable
+{
+    public MaterialPhysics[] detectedMaterials;
+    [SerializeField] private ActivableAlarm linkedAlarm;
+
+    public override ActivationType GetActivationType()
+    {
+        return ActivationType.Other;
+    }
+
+    protected override void SetState(bool state, CharacterManager characterActivating, bool alertAtActivate)
+    {
+        if (state)
+            foreach (MaterialPhysics material in detectedMaterials)
+            {
+                try
+                {
+                    if (characterActivating != null)
+                        if (material == ((PlayerManager)characterActivating).inventory.storedItem.GetComponent<MaterialWithSound>().materialPhysics)
+                        {
+                            //linkedAlarm.SwitchState(characterActivating);
+                            linkedAlarm.ActivateAlarm();
+                        }
+                }
+                catch (System.InvalidCastException)
+                { }
+                catch (System.NullReferenceException)
+                { }
+
+            }
+    }
+}
