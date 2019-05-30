e53e89c72ff6f05f8757d986a5db6417509f95e5
gillemp <guplem@gmail.com>
2019-05-30 11:28:51 +0200

    Added set checkpint cheat and checkpoints cheats window

---
+++ Proj2/Assets/Scripts/Miscellanea/Editor/SetCheckPointCheat.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class SetCheckPointCheat : EditorWindow
+{
+    [MenuItem("Bunker Bound/Cheats/Set checkpoint")]
+    public static void ShowWindow()
+    {
+        GetWindow<SetCheckPointCheat>("Checkpoint cheats");
+    }
+
+    string checkPointNumberString = "-1";
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Write the checkpoint number: ", EditorStyles.boldLabel);
+
+        checkPointNumberString = EditorGUILayout.TextField("Zone number", checkPointNumberString);
+
+        if (GUILayout.Button("Set new CheckPoint"))
+        {
+            int checkPointNumber = 0;
+
+            if (int.TryParse(checkPointNumberString, out checkPointNumber))
+            {
+                if (!Application.isPlaying)
+                {
+                    Debug.LogError("The game is not in play mode.");
+                    return;
+                }
+
+                CheckPoint[] CheckPoints = (CheckPoint[])FindObjectsOfType(typeof(CheckPoint));
+                foreach (CheckPoint ch in CheckPoints)
+                {
+                    if (ch.zone == checkPointNumber)
+                    {
+                        GameManager.Instance.CheckPointReached(ch);
+                        Debug.Log("Forced checkpoint " + ch.zone);
+                        return;
+                    }
+                }
+                Debug.LogError("CheckPoint '" + checkPointNumber + "' not found");
+
+
+            }
+            else
+            {
+                Debug.LogError("The zone is not a numebr");
+            }
+
+        }
+
+        if (GUILayout.Button("Hit player"))
+        {
+            Cheats.HitPlayer();
+        }
+
+        if (GUILayout.Button("Reset until last checkpoint"))
+        {
+            Cheats.ResetTilLastCheckpoint();
+        }
+    }
+}
